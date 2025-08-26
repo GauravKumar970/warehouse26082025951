b6a113d1fa8c6463119221fb37e3e321c9d5d116
Gaurav Kumar <G19065@astra.xlri.ac.in>
2025-08-26 12:29:38 +0530

    Update slotting_agent.py

--- slotting_agent.py
+++ slotting_agent.py
@@ -2,15 +2,10 @@ import pandas as pd
 
 def recommend_slotting(df_analyzed):
     df_optimized = df_analyzed.copy()
-    
-    # Sort by category (A, B, C) and then by daily demand
     df_optimized['ABC_Category_Sort'] = df_optimized['ABC_Category'].map({'A': 1, 'B': 2, 'C': 3})
     df_optimized = df_optimized.sort_values(by=['ABC_Category_Sort', 'Daily_Demand'], ascending=[True, False])
-    
-    # Assign new optimized locations (e.g., LOC_A01, LOC_B01, etc.)
     df_optimized['New_Location'] = 'TBD'
     
-    # Simple example of assigning new locations based on category
     a_count = 1
     b_count = 1
     c_count = 1
@@ -22,7 +17,7 @@ def recommend_slotting(df_analyzed):
         elif row['ABC_Category'] == 'B':
             df_optimized.loc[index, 'New_Location'] = f"LOC_B{b_count:02}"
             b_count += 1
-        else: # C
+        else:
             df_optimized.loc[index, 'New_Location'] = f"LOC_C{c_count:02}"
             c_count += 1
             
